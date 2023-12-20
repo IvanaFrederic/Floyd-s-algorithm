0686b08e62e610adee12b2d5f8e48e53971aa167
IvanaFrederic <154450634+IvanaFrederic@users.noreply.github.com>
2023-12-21 00:38:59 +0530

    floyds.py

--- floyds.py
+++ floyds.py
@@ -2,23 +2,26 @@ INF = float('inf')
 
 def floyd_warshall(graph):
     vertices = len(graph)
-    
-    # Initialize the distance matrix with direct edge weights
     distance_matrix = [[INF] * vertices for _ in range(vertices)]
+    next_matrix = [[None] * vertices for _ in range(vertices)]
+
     for i in range(vertices):
         for j in range(vertices):
             if i == j:
                 distance_matrix[i][j] = 0
+                next_matrix[i][j] = None
             elif graph[i][j] != 0:
                 distance_matrix[i][j] = graph[i][j]
-    
-    # Update the distance matrix using all possible intermediate vertices
+                next_matrix[i][j] = j
+
     for k in range(vertices):
         for i in range(vertices):
             for j in range(vertices):
-                distance_matrix[i][j] = min(distance_matrix[i][j], distance_matrix[i][k] + distance_matrix[k][j])
+                if distance_matrix[i][k] + distance_matrix[k][j] < distance_matrix[i][j]:
+                    distance_matrix[i][j] = distance_matrix[i][k] + distance_matrix[k][j]
+                    next_matrix[i][j] = next_matrix[i][k]
 
-    return distance_matrix
+    return distance_matrix, next_matrix
 
 # Example usage:
 graph = [
@@ -28,9 +31,16 @@ graph = [
     [2, INF, INF, 0]
 ]
 
-result_matrix = floyd_warshall(graph)
+distances_matrix, next_matrix = floyd_warshall(graph)
 
-# Print the result matrix
-print("Shortest distances between all pairs:")
-for row in result_matrix:
+print("Graph adjacency matrix:")
+for row in graph:
+    print(row)
+
+print("\nShortest distances between all pairs:")
+for row in distances_matrix:
+    print(row)
+
+print("\nNext vertices in the shortest paths:")
+for row in next_matrix:
     print(row)
